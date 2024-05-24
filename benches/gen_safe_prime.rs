32d079b3afcf4fc605c91875e7f92f08caeefd5a
Andrew Whitehead <cywolf@gmail.com>
2024-05-23 18:40:42 -0700

    add bench of prime::new
    Signed-off-by: Andrew Whitehead <cywolf@gmail.com>

--- benches/gen_safe_prime.rs
+++ benches/gen_safe_prime.rs
@@ -1,8 +1,11 @@
 use criterion::{criterion_group, criterion_main, Criterion};
-use glass_pumpkin::safe_prime::new;
+use glass_pumpkin::{prime, safe_prime};
 
 fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("gen_safe_prime 1", |b| b.iter(|| new(256).unwrap()));
+    c.bench_function("gen_prime u256", |b| b.iter(|| prime::new(256).unwrap()));
+    c.bench_function("gen_safe_prime u256", |b| {
+        b.iter(|| safe_prime::new(256).unwrap())
+    });
 }
 
 criterion_group!(benches, criterion_benchmark);
